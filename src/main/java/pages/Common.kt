fea163d97fa9abe6d362e8f9e17628fec6c6d19f
Grzegorz Holak <holi87@gmail.com>
2019-01-27 11:43:41 +0100

    press button by text

--- src/main/java/pages/Common.kt
+++ src/main/java/pages/Common.kt
@@ -3,7 +3,8 @@ package pages
 import Config
 import net.bytebuddy.utility.RandomString
 import org.openqa.selenium.By
-import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable
+import org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated
 import org.openqa.selenium.support.ui.WebDriverWait
 
 open class Common {
@@ -16,15 +17,19 @@ open class Common {
         Config.driver.quit()
     }
 
-    val webDriverWait = WebDriverWait(Config.driver, 5)
+    private val webDriverWait = WebDriverWait(Config.driver, 5)
 
     fun pressButtonById(id: String) {
-        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id(id))).click()
+        webDriverWait.until(elementToBeClickable(By.id(id))).click()
+    }
+
+    fun pressButtonByText(text: String) {
+        webDriverWait.until(elementToBeClickable(By.xpath("//button[contains(text(),$text)]")))
     }
 
     fun checkSolution(): String {
         pressButtonById("end")
-        return webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#trail code"))).text
+        return webDriverWait.until(visibilityOfElementLocated(By.cssSelector("#trail code"))).text
     }
 
     fun randomStringGenerator(numberOfChars: Int): String {
--- src/test/java/pages/Antycaptcha.kt
+++ src/test/java/pages/Antycaptcha.kt
@@ -14,18 +14,16 @@ class AntycaptchaSpec : Spek({
     describe("Starter") {
         val goodAnswer = "OK. Good answer"
         describe("Cwiczenie1") {
-            val exercise1 = Exercises()
-            beforeEachTest { exercise1.open(Config.antycaptchaUrl + exercise1.url) }
+            val exercise = Exercises()
+            beforeEachTest { exercise.open(Config.antycaptchaUrl + exercise.url) }
             it("Kliknij B1B2B1 a potem sprawdź (positive one)") {
-                exercise1.pressB1()
-                exercise1.pressB2()
-                exercise1.pressB1()
-                exercise1.checkSolution() shouldBe goodAnswer
+
+                exercise.checkSolution() shouldBe goodAnswer
             }
             it("Kliknij B1B2 a potem sprawdź (negative one)") {
-                exercise1.pressB1()
-                exercise1.pressB2()
-                exercise1.checkSolution() shouldNotBe goodAnswer
+                exercise.pressB1()
+                exercise.pressB2()
+                exercise.checkSolution() shouldNotBe goodAnswer
             }
         }
 
